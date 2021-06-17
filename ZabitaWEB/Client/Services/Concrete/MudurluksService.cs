bb16bb93c84c8b3be953633f308018a5fc6331ec
Abdullah ÖZER <ozerabdullah11@gmail.com>
2021-06-17 15:48:26 +0300

    son1

--- ZabitaWEB/Client/Services/Concrete/MudurluksService.cs
+++ ZabitaWEB/Client/Services/Concrete/MudurluksService.cs
@@ -1,6 +1,46 @@
-﻿namespace ZabitaWEB.Client.Services.Concrete
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Net.Http.Json;
+using System.Threading.Tasks;
+using Zabita.Entities.Concrete;
+using ZabitaWEB.Client.Services.Abstract;
+
+namespace ZabitaWEB.Client.Services.Concrete
 {
-    public class MudurluksService
+    public class MudurluksService : IMudurluksService
     {
+        private readonly HttpClient _httpClient;
+        public MudurluksService(HttpClient httpClient)
+        {
+            _httpClient = httpClient;
+        }
+
+        public Task<IActionResult> DeleteMudurluk(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<ActionResult<Mudurluk>> GetMudurluk(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public async Task<ActionResult<IEnumerable<Mudurluk>>> GetMudurluks()
+        {
+            return await _httpClient.GetFromJsonAsync<List<Mudurluk>>("/api/Mudurluks");
+        }
+
+        public Task<ActionResult<Mudurluk>> PostMudurluk(Mudurluk mudurluk)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<IActionResult> PutMudurluk(int id, Mudurluk mudurluk)
+        {
+            throw new NotImplementedException();
+        }
     }
 }
--- ZabitaWEB/Server/Startup.cs
+++ ZabitaWEB/Server/Startup.cs
@@ -18,7 +18,7 @@ namespace ZabitaWEB.Server
         public Startup(IConfiguration configuration)
         {
             //ZabitaDatabaseContext zabitaDatabaseContext = new ZabitaDatabaseContext();
-            //zabitaDatabaseContext.Database.EnsureDeleted();
+          //  zabitaDatabaseContext.Database.EnsureDeleted();
             //zabitaDatabaseContext.Database.EnsureCreated();
             Configuration = configuration;
         }
