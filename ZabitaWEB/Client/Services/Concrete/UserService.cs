cc0d2edb9e34cd7cb4bf685f9e7858da325ee574
_oguz <oguzhan_687@hotmail.com>
2021-08-10 14:43:35 +0300

    ibb-commit

---
+++ ZabitaWEB/Client/Services/Concrete/UserService.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Net.Http.Json;
+using System.Threading.Tasks;
+using Zabita.Entities.Concrete;
+using ZabitaWEB.Client.Services.Abstract;
+
+namespace ZabitaWEB.Client.Services.Concrete
+{
+    public class UserService:IUserService
+    {
+        private readonly HttpClient _httpClient;
+
+        public UserService(HttpClient httpClient)
+        {
+            _httpClient = httpClient;
+        }
+        public async Task<HttpResponseMessage> GetUser()
+        {
+            return await _httpClient.GetAsync("/api/User");
+        }
+    }
+}
